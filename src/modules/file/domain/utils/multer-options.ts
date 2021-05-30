8b5db28f14b435cdf08a36a7073fc3e5d42c537e
dubu4050 <dubu4050@naver.com>
2021-05-30 22:23:55 +0900

    Fix: multer option
    - multer option 내부 누락된 callback 함수 수정

--- src/modules/file/domain/utils/multer-options.ts
+++ src/modules/file/domain/utils/multer-options.ts
@@ -4,16 +4,18 @@ import { existsSync, mkdirSync } from 'fs';
 import encryptFileName from './encrypt-file-name';
 
 export const multerOptions = {
-  fileFilter: (request, file, callback) => {
-    if (!file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
-      callback(
-        new HttpException(
-          'Only image files are allowed!',
-          HttpStatus.BAD_REQUEST,
-        ),
-        false,
-      );
-    }
+    fileFilter: (request, file, callback) => {
+      if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+        callback(null, true);
+      } else {
+        callback(
+          new HttpException(
+            'Only image files are allowed!',
+            HttpStatus.BAD_REQUEST,
+          ),
+          false,
+        );
+      }
   },
 
   storage: diskStorage({
